fad81ead6961143084ecc708709378a058fd1a04
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2022-10-27 12:45:16 +0200

    firebaseconfig

---
+++ src/routes/login.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { initializeApp } from "@firebase/app";
+    
+    const firebaseConfig = {
+        apiKey: "AIzaSyCnM42APKThIKwvesEAwoXmE6DLbHlk6Z0",
+        authDomain: "stepup-health.firebaseapp.com",
+        projectId: "stepup-health",
+        storageBucket: "stepup-health.appspot.com",
+        messagingSenderId: "155380053451",
+        appId: "1:155380053451:web:870d0809c0a08aa580324e",
+        measurementId: "G-5YFZ569QL8"
+    };
+
+    let email = "";
+    let password = "";
+
+    const login = () => {};
+
+    const logout = () => {};
+</script>
+
+<input type="email" id="email" placeholder="email" bind:value={email}>
+<input type="password" id="password" placeholder="password" bind:value={password}>
+<button on:click={login}>Login</button>
+<button on:click={logout}>Logout</button>
