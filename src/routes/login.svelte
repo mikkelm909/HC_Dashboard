750757c1a4514475155e2223c4f175b7e66d8005
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2022-10-28 10:06:02 +0200

    Made login system

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+    import { onMount } from "svelte"
     import { initializeApp } from "@firebase/app";
-    
+    import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged, type User } from "firebase/auth";
+    //https://www.youtube.com/watch?v=PXf0t6Id7i0&ab_channel=IvanSantos
     const firebaseConfig = {
         apiKey: "AIzaSyCnM42APKThIKwvesEAwoXmE6DLbHlk6Z0",
         authDomain: "stepup-health.firebaseapp.com",
@@ -11,15 +13,50 @@
         measurementId: "G-5YFZ569QL8"
     };
 
+    const app = initializeApp(firebaseConfig);
+
+    const auth = getAuth(app);
+
+    let user: User | null;
     let email = "";
     let password = "";
+    
+    const login = () => {
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+            user = userCredentials.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode + errorMessage);
+        })
+    };
 
-    const login = () => {};
+    const logout = async () => {
+        signOut(auth).then(() => {
+            user = null;
+            console.log("You are logged out!");
+        }).catch((error) => {
+            console.log("Error logging out!")
+        })
+    };
 
-    const logout = () => {};
+    onMount(async () => {
+        onAuthStateChanged(auth, (newUser) => {
+            user = newUser;
+        })
+    })
 </script>
 
-<input type="email" id="email" placeholder="email" bind:value={email}>
-<input type="password" id="password" placeholder="password" bind:value={password}>
-<button on:click={login}>Login</button>
-<button on:click={logout}>Logout</button>
+
+
+{#if user}
+    <p>Signed in!</p>
+    <button on:click={logout}>Logout</button>
+{:else}
+    <input type="email" id="email" placeholder="email" bind:value={email}>
+    <input type="password" id="password" placeholder="password" bind:value={password}>
+    <button on:click={login}>Login</button>
+{/if}
