bcb0c80d9f2f3848d708121b6b9028b3c64edfc5
mikkelm909 <mikkelwnm@gmail.com>
2022-11-03 15:58:39 +0100

    Setup

---
+++ src/db/Collections/PatientData.ts
@@ -0,0 +1,3 @@
+import db from "$db/db";
+
+export const mongoData = db.collection("PatientData") //defines the mongo collection
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,6 +29,3 @@
         </table>
     </div>
 </ProtectedLayout>
-
-
-
