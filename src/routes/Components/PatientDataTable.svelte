1a3dd069597ac77aeaeaa73901a4c86be45d1ad4
mikkelm909 <mikkelwnm@gmail.com>
2022-11-04 11:45:06 +0100

    Patient data

--- src/routes/Components/PatientDataTable.svelte
+++ src/routes/Components/PatientDataTable.svelte
@@ -1,7 +1,62 @@
-<script>
-
+<script lang="ts">
+    export let patients: any[] = []
 </script>
 
-<table>
-    
+<table width="500" border="10">
+    <tr>
+
+        <th>Mikkel Meiling</th>
+        {#each patients as patient}
+        <th>{patient.Date.getDay()}-{patient.Date.toLocaleString('default', { month: 'short' })}</th> <!-- Getting the date for the session -->
+    {/each}  
+    </tr>
+    <tr>
+        <th>Breathing Rate (avg/min)</th>
+        {#each patients as patient}
+        <th>{patient.BreathingRate}</th>
+    {/each}  
+    </tr>
+    <tr>
+        <th>BreathingDepth (avg%/min)</th>
+        {#each patients as patient}
+        <th>{patient.BreathingDepth}</th>
+    {/each}  
+    </tr>
+    <tr>
+        <th>Oxygen (SPO2) (avg/min)</th>
+        {#each patients as patient}
+        <th>{patient.SPO2}</th>
+    {/each}  
+    </tr>
+    <tr>
+        <th>Caughing count (session)</th>
+        {#each patients as patient}
+        <th>{patient.CaughingCount}</th>
+    {/each}  
+    </tr>
+    <tr>
+        <th>HeartRate (avg/min)</th>
+        {#each patients as patient}
+        <th>{patient.HeartRate}</th>
+    {/each}  
+    </tr>
+    <tr>
+        <th>HRV (avg)</th>
+        {#each patients as patient}
+        <th>{patient.HRV}</th>
+    {/each}  
+    </tr>
+    <tr>
+        <th>Arythmia count (during session)</th>
+        {#each patients as patient}
+        <th>{patient.ArythmiaCount}</th>
+    {/each}  
+    </tr>
+    <tr>
+        <th>BodyTemperature (avg/session)</th>
+        {#each patients as patient}
+        <th>{patient.BodyTemperature}</th>
+    {/each}  
+    </tr>
 </table>
+
---
+++ src/routes/PatientData/+page.server.ts
@@ -0,0 +1,28 @@
+import type { PageServerLoad } from './$types';
+import { PatientData } from '$db/Collections/PatientData';
+
+
+
+export const load: PageServerLoad = async function(){
+    const data = await PatientData.find({ patientId:  "635f8960d51013f66af52a8b" }, {
+        projection: {BreathingRate: 1, _id: 1, BreathingDepth: 1, SPO2: 1, CaughingCount: 1, HeartRate: 1, HRV: 1, ArythmiaCount: 1, BodyTemperature: 1, Date: 1}}).toArray(); //for some reason it will not return objects with new ObjectId("") so we map it out
+
+    const PatientArray = data.map(s => { return { 
+        id: s._id.toString(), 
+        BreathingRate: s.BreathingRate,
+        BreathingDepth: s.BreathingDepth,
+        SPO2: s.SPO2,
+        CaughingCount: s.CaughingCount,
+        HeartRate: s.HeartRate,
+        HRV: s.HRV,
+        ArythmiaCount: s.ArythmiaCount,
+        BodyTemperature: s.BodyTemperature,
+        Date: new Date(s.Date) 
+    } })
+
+    return {
+        PatientData: PatientArray
+    }
+    
+    
+}
