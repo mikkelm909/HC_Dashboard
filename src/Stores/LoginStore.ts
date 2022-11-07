451ca31f606012ccc476647b027d220e0272c87c
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2022-11-07 13:03:03 +0100

    Update LoginStore.ts

--- src/Stores/LoginStore.ts
+++ src/Stores/LoginStore.ts
@@ -1,17 +1,15 @@
-import { browser } from "$app/environment";
-import { writable } from "svelte/store";
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
 
 //we use the browser to avoid erros if this is run before the server side render
-if(browser){ 
-    var localUser = localStorage.getItem("storeUser")
+if (browser) {
+	var localUser = localStorage.getItem('storeUser');
 }
 
-export const storeUser = writable(localUser)
+export const storeUser = writable(localUser);
 
-if(browser)
-{
-    storeUser.subscribe(value => {
-        localStorage.setItem("storeUser", value)
-    })
+if (browser) {
+	storeUser.subscribe((value) => {
+		localStorage.setItem('storeUser', value);
+	});
 }
-
