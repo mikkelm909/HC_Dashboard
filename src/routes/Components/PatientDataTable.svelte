e2d66136f186dd8f91bf2ebd1260b09d1961c744
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2022-11-11 11:21:07 +0100

    patient name

--- src/routes/Components/PatientDataTable.svelte
+++ src/routes/Components/PatientDataTable.svelte
@@ -1,49 +1,40 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 	export let patient: any[] = [];
 
+	let startDate: Date = new Date('2000-11-10');
+	let endDate: Date = new Date('3000-11-10');
 
-	let startDate: Date = new Date("2000-11-10");
-	let endDate: Date = new Date("3000-11-10");
+	$: formatedStart = new Date(startDate);
+	$: formatedEnd = new Date(endDate);
 
-
-	$: formatedStart = new Date(startDate)
-	$: formatedEnd = new Date(endDate)
-	
-	var filteredPatient: any[] = []
-	function getDataByDates(){
-		filteredPatient = []
-		patient.forEach(p => {
-			if(p.Date.getTime() <= formatedEnd.getTime() && p.Date.getTime() >= formatedStart.getTime())
-				filteredPatient.push(p)
+	var filteredPatient: any[] = [];
+	function getDataByDates() {
+		filteredPatient = [];
+		patient.forEach((p) => {
+			if (p.Date.getTime() <= formatedEnd.getTime() && p.Date.getTime() >= formatedStart.getTime())
+				filteredPatient.push(p);
 		});
-
 	}
 
 	onMount(() => {
-		
-			filteredPatient = patient
-		
-	})
+		filteredPatient = patient;
+	});
 </script>
-
-
-
-
 
 <p>Start date</p>
 <label>
-	<input type=date bind:value={startDate}>
+	<input type="date" bind:value={startDate} />
 </label>
 <p>End date</p>
 <label>
-	<input type=date bind:value={endDate}>
+	<input type="date" bind:value={endDate} />
 </label>
 
 <button on:click={getDataByDates}>Show data for dates</button>
 <table width="500" border="10">
 	<tr>
-		<th>{patient[0].id}</th>
+		<th>{patient[0].name}</th>
 		{#each filteredPatient as patient}
 			<th>{patient.Date.toLocaleString()}</th>
 			<!-- Getting the date for the session -->
