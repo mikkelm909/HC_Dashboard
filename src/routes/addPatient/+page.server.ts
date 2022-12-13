08228dbb42aabb9c8f7261c564a8d026d6f55d0e
mikkelm909 <mikkelwnm@gmail.com>
2022-12-13 15:19:20 +0100

    start - sending test emails

--- src/routes/addPatient/+page.server.ts
+++ src/routes/addPatient/+page.server.ts
@@ -1,5 +1,12 @@
 import type { PageServerLoad } from ".svelte-kit/types/src/routes/$types";
+import { EmailAuthCredential } from "firebase/auth";
 
 export const load: PageServerLoad = async function(){
+
+    function sendMail() {
+        const serviceId = "nextcloud_email"
+        const templateId = "template_a4kvakl"
+
+    }
 
 }
---
+++ src/routes/addPatient/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import emailjs from '@emailjs/browser';
+  
+    function sendEmail(e) {
+      emailjs.sendForm('nextcloud_email', 'template_a4kvakl', e.target, 'pYAFgEMR9CybRdwAz')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
+    }
+  </script>
+  
+  <form on:submit|preventDefault={sendEmail}>
+    <label>Name</label>
+    <input type="text" name="to_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
