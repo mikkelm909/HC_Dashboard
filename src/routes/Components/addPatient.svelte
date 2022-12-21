e4731553cceb95c2e59d13f6e6e63e0fa7287295
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2022-12-21 09:22:43 +0100

    navbar & protectedlayout

--- src/routes/Components/addPatient.svelte
+++ src/routes/Components/addPatient.svelte
@@ -1,29 +1,27 @@
 <script lang="ts">
-	import { storeUser } from "$protectedUser";
-	import { json } from "@sveltejs/kit";
-	import { onMount } from "svelte";
-  let uid = ""
+	import { storeUser } from '$protectedUser';
+	import { json } from '@sveltejs/kit';
+	import { onMount } from 'svelte';
+	import Navbar from './navbar.svelte';
+	import ProtectedLayout from './ProtectedLayout.svelte';
+	let uid = '';
 
-  onMount(async () => {
-    let userData = $storeUser
-    uid = JSON.parse(userData).uid
-  })
-
+	onMount(async () => {
+		let userData = $storeUser;
+		uid = JSON.parse(userData).uid;
+	});
 </script>
 
+<Navbar />
+<ProtectedLayout>
+	<div class="container-fluid" style="margin-top: 80px">
+		<h1>Add a patient</h1>
+		<h5>Send an e-mail invite</h5>
+		<form method="POST">
+			<input type="string" name="email" />
+			<input type="hidden" name="uid" bind:value={uid} />
 
-<h1>Add a patient</h1>
-<h5>Send an e-mail invite</h5>
-<form method="POST">
-    <input
-    type="string"
-    name="email"
-  />  
-    <input
-    type="hidden"
-    name="uid"
-    bind:value={uid}
-  />  
-
-	<button class="btn btn-outline-primary">Send email authetication</button>
-</form>
+			<button class="btn btn-outline-primary">Send email authetication</button>
+		</form>
+	</div>
+</ProtectedLayout>
